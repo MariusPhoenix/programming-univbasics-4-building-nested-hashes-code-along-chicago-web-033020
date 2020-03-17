9d85b1f36730c7d689323a1ea09a48ec7c284157
Marius Phoenix <phoenixzenomarius@gmail.com>
2020-03-17 03:38:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,10 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         { :name => "Benvolio"
-          :age => ""
+          :age => "17"
+          :attitude => "worried"
+        }
+        { :name => 
       ]
    }, 
    :capulet => {
